c165bd56efe658e6eb8518df074f3ec668711959
Chris Mottram <C.Mottram@ljmu.ac.uk>
2004-01-30 16:21:13 +0000

    Changed logging bits to be different from Java layer.

--- include/object.h
+++ include/object.h
@@ -1,5 +1,5 @@
 /* object.h
-** $Header: /space/home/eng/cjm/cvs/libdprt-object/include/object.h,v 1.3 2004-01-29 13:29:04 cjm Exp $
+** $Header: /space/home/eng/cjm/cvs/libdprt-object/include/object.h,v 1.4 2004-01-30 16:21:13 cjm Exp $
 */
 #ifndef OBJECT_H
 #define OBJECT_H
@@ -38,26 +38,31 @@
  */
 #define ONE_SECOND_MS        (1000)                                
 
+/* Logging bits.
+** Note the DpRt java layer reserves bits 0-7 inclusive.
+** Note libdprt_rjs reserves bits 8-15 inclusive (currently unused).
+** Note libdprt_object reserves bits 16-23 inclusive.
+*/
 /**
  * Value to pass into logging calls, used to log general execution.
  */
-#define OBJECT_LOG_BIT_GENERAL       (1<<0)
+#define OBJECT_LOG_BIT_GENERAL       (1<<16)
 /**
  * Value to pass into logging calls, used to log object changing code.
  */
-#define OBJECT_LOG_BIT_OBJECT        (1<<1)
+#define OBJECT_LOG_BIT_OBJECT        (1<<17)
 /**
  * Value to pass into logging calls, used to log FWHM code.
  */
-#define OBJECT_LOG_BIT_FWHM           (1<<2)
+#define OBJECT_LOG_BIT_FWHM          (1<<18)
 /**
  * Value to pass into logging calls, used to log pixel (of object) code.
  */
-#define OBJECT_LOG_BIT_PIXEL          (1<<3)
+#define OBJECT_LOG_BIT_PIXEL         (1<<19)
 /**
  * Value to pass into logging calls, used to log point list (internal list recursion) code.
  */
-#define OBJECT_LOG_BIT_POINT          (1<<4)
+#define OBJECT_LOG_BIT_POINT         (1<<20)
 
 /* structures */
 /**
